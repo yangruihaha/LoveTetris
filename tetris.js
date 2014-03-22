8669981d68980e3cc47083512539ec892a267c42
yangruihaha <ryang.pure@gmail.com>
2014-03-22 21:39:58 +0800

    temp

--- tetris.js
+++ tetris.js
@@ -27,12 +27,21 @@ var shape = [	//TShape index:0-3
 			];
 			
 var current_element = {
-	shape_index: 0,
+	shape_index: 4,
 	coordinate: [0,-1]
-}			
+}		
 			
 var c=document.getElementById("myCanvas");
 var cxt=c.getContext("2d");
+
+/* to record the element has fallen */
+var occupancy = new Array();
+for(var i=0; i<c.width/20; i++){
+	occupancy[i] = new Array();
+	for(var j=0; j<c.height/20; j++){
+		occupancy[i][j] = 0;
+	}
+}
 
 function fillElement(index, coordinate, color){
 	for (var i = 0; i<shape[index].length;i++){
@@ -42,14 +51,20 @@ function fillElement(index, coordinate, color){
 	}
 }
 
-/*
-function clearElement(index, coordinate){
+function fillOccupancy(index, coordinate, color){
 	for (var i = 0; i<shape[index].length;i++){
-		cxt.fillStyle="#666666";
-		cxt.fillRect((coordinate[0]+shape[index][i][0])*20,(coordinate[1]+shape[index][i][1])*20,18,18);
+		//cxt.fillStyle="#99FF66";
+		occupancy[coordinate[0]+shape[index][i][0]][coordinate[1]+shape[index][i][1]] = 1;
+	}
+	for (var i = 0; i<occupancy.length;i++){
+		for (var j = 0; j<occupancy[i].length;j++){
+			cxt.fillStyle=color;
+			if(occupancy[i][j] == 1){
+				cxt.fillRect(i*20, j*20,18,18);
+			}
+		}
 	}
 }
-*/
 
 function mmMatrix(m, mm, xy){
 	if(xy == "x" && mm == "min"){
@@ -82,10 +97,88 @@ function mmMatrix(m, mm, xy){
 	}
 }
 
+function collisionCheck(index, coordinate, direct){
+	for (var i = 0; i<shape[index].length;i++){
+		if(direct == "down"){
+			if(occupancy[coordinate[0]+shape[index][i][0]][coordinate[1]+shape[index][i][1]+1] == 1){
+				return true;
+			}
+		}
+		else if(direct == "left"){
+			if(occupancy[coordinate[0]+shape[index][i][0]-1][coordinate[1]+shape[index][i][1]] == 1){
+				return true;
+			}
+		}
+		else if(direct == "right"){
+			if(occupancy[coordinate[0]+shape[index][i][0]+1][coordinate[1]+shape[index][i][1]] == 1){
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+function eraseLine(line){
+	cxt.fillStyle="#666666";
+	cxt.fillRect(0,line*20,c.width,20);
+
+	for(var j=line; j>=0; j--){
+		for(var i=0; i<occupancy.length; i++){
+			if(j > 0){
+				occupancy[i][j] = occupancy[i][j-1];
+			}
+			else{
+				occupancy[i][j] = 0;
+			}
+		}
+	}
+	for (var i = 0; i<occupancy.length;i++){
+		for (var j = 0; j<occupancy[i].length;j++){
+			if(occupancy[i][j] == 1){
+				cxt.fillStyle='#868866';
+				cxt.fillRect(i*20, j*20,18,18);
+			}
+			else{
+				cxt.fillStyle='#666666';
+				cxt.fillRect(i*20, j*20,18,18);
+			}
+		}
+	}
+}
+
+function erase(){
+	for(var j =0; j<occupancy[0].length; j++){
+		for(var i=0; i<occupancy.length; i++){
+			if(occupancy[i][j] == 0){
+				break;
+			}
+		}
+		if( i == occupancy.length){
+			eraseLine(j);
+		}
+	}
+}
+
+function allCheck(index, coordinate){
+	return	mmMatrix(shape[index], 'max', 'y') + coordinate[1] + 1 < c.height/20 &&
+			//!collisionCheck(index, coordinate, 'down') &&
+			mmMatrix(shape[index], 'min', 'x') + coordinate[0] - 1 >= 0 &&
+			//!collisionCheck(index, coordinate, 'left') &&
+			mmMatrix(shape[index], 'max', 'x') + coordinate[0] + 1 < c.width/20;
+			//!collisionCheck(index, coordinate, 'right');
+}
+
+
 function changeCurrentElement(keycode){
 	if (keycode == 38){
 		if(current_element.shape_index >=0 && current_element.shape_index <=11){
-			current_element.shape_index = Math.floor(current_element.shape_index/4)*4 + (current_element.shape_index+1)%4;
+			//current_element.shape_index = Math.floor(current_element.shape_index/4)*4 + (current_element.shape_index+1)%4;
+			temp_index = current_element.shape_index = Math.floor(current_element.shape_index/4)*4 + (current_element.shape_index+1)%4;
+			console.log(temp_index);
+			if(allCheck(temp_index, current_element.coordinate)){
+
+				current_element.shape_index = temp_index;
+			}
 		}
 		else if(current_element.shape_index >=12 && current_element.shape_index <=17){
 			if(current_element.shape_index % 2 ==0){
@@ -96,16 +189,23 @@ function changeCurrentElement(keycode){
 			}
 		}
 	}
-	else if(keycode == 40 && mmMatrix(shape[current_element.shape_index], 'max', 'y') + current_element.coordinate[1] + 1 < c.height/20){
+	else if(keycode == 40 && mmMatrix(shape[current_element.shape_index], 'max', 'y') + current_element.coordinate[1] + 1 < c.height/20 &&
+			!collisionCheck(current_element.shape_index, current_element.coordinate, 'down') ){
 		current_element.coordinate[1] = current_element.coordinate[1] + 1;
 	}
-	else if(keycode == 37 && mmMatrix(shape[current_element.shape_index], 'min', 'x') + current_element.coordinate[0] - 1 >= 0){	//left move
+	else if(keycode == 37 && mmMatrix(shape[current_element.shape_index], 'min', 'x') + current_element.coordinate[0] - 1 >= 0 &&
+			!collisionCheck(current_element.shape_index, current_element.coordinate, 'left') ){	//left move
 		current_element.coordinate[0] = current_element.coordinate[0] - 1;
 	}
-	else if(keycode == 39 && mmMatrix(shape[current_element.shape_index], 'max', 'x') + current_element.coordinate[0] + 1 < c.width/20 ){	//right move	
+	else if(keycode == 39 && mmMatrix(shape[current_element.shape_index], 'max', 'x') + current_element.coordinate[0] + 1 < c.width/20 &&
+			!collisionCheck(current_element.shape_index, current_element.coordinate, 'right') ){	//right move	
 		current_element.coordinate[0] = current_element.coordinate[0] + 1;
 	}
-	else if(mmMatrix(shape[current_element.shape_index], 'max', 'y') + current_element.coordinate[1] >= c.height/20 - 1){
+	else if(mmMatrix(shape[current_element.shape_index], 'max', 'y') + current_element.coordinate[1] >= c.height/20 - 1 ||
+			collisionCheck(current_element.shape_index, current_element.coordinate, 'down')){
+		fillOccupancy(current_element.shape_index, current_element.coordinate,'#868866');
+		erase();
+
 		current_element.coordinate[0] = 0;
 		current_element.coordinate[1] = -1;
 	}
